d4ac3c662a1dff9cba84240c9762852c408220a9
Michael Klopf <klopf.michael@gmail.com>
2014-10-08 07:19:29 +0200

    Included header in each html file.

--- static/js/views/page.js
+++ static/js/views/page.js
@@ -22,18 +22,10 @@ var PageComponent = React.createClass({
   },
   render : function() {
     return (
-      <div id="content">
-        <div id="wrap">
-          <HeaderComponent router={this.props.router} />
-          <div className="container">
-            <div className="page-header">
-              <h1>Welcome to the template.</h1>
-              <p className="lead">This is a lead:</p>
-              <ExampleComponent data={collection} />
-            </div>
-          </div>
-        </div>
-        <FooterComponent />
+      <div className="page-header">
+        <h1>Welcome to the template.</h1>
+        <p className="lead">This is a lead:</p>
+        <ExampleComponent data={collection} />
       </div>
     );
   }
